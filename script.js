d58bac588b006a327727ee5026a6d2974056b321
Agnieszka Jankowska <agn_jankowska@wp.pl>
2020-08-13 11:29:12 +0200

    expence website

---
+++ script.js
@@ -0,0 +1,8 @@
+n =  new Date();
+y = n.getFullYear();
+m = n.getMonth() + 1;
+if(m<10) m = '0'+m;
+d = n.getDate();
+if(d<10) d = '0'+d;
+
+document.getElementById("currentDate").value = y + "-" + m + "-" + d;
